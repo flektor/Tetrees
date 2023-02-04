3229e7313b9eb64dbc9e515b5c9241d92140711b
flektor <gdrosin@gmail.com>
2023-02-04 15:09:42 +0100

    Broken rotation

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -1,10 +1,35 @@
- using UnityEngine; 
+using UnityEngine;
 
 public class Drag : MonoBehaviour
 {
-      
+
+
+
+    private void Update()
+    {
+        if (placementState != RootPlacementState.Rotating) return;
+
+
+
+        var mousePosition = Camera.main.WorldToScreenPoint(Input.mousePosition);
+
+        var delta = mousePosition - _collider.transform.position;
+         
+        var angle = Vector3.Angle(Vector3.up, delta);
+
+      //  transform.Rotate(transform.position, angle);
+
+        transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, angle);
+
+        Debug.DrawLine(_collider.transform.position, mousePosition);
+
+
+
+    }
+
     void OnMouseDown()
     {
+        placementState = RootPlacementState.Dragging;
         _initTransformPosition = transform.position;
         _dropPosition = _initTransformPosition;
         _initMousePosition = Input.mousePosition - Camera.main.WorldToScreenPoint(_initTransformPosition);
@@ -14,7 +39,6 @@ public class Drag : MonoBehaviour
     {
         if (_collider == null)
         {
-
             transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition - _initMousePosition);
             return;
         }
@@ -24,8 +48,13 @@ public class Drag : MonoBehaviour
 
     void OnMouseUp()
     {
-        transform.position = _dropPosition; 
+        transform.position = _dropPosition;
+        if (_isAttached)
+        {
+            placementState = RootPlacementState.Rotating;
+        }
     }
+
 
 
     void OnTriggerEnter(Collider other)
@@ -33,8 +62,10 @@ public class Drag : MonoBehaviour
         if (!other.CompareTag("Node"))
         {
             _dropPosition = _initTransformPosition;
+            _isAttached = false;
             return;
         }
+        _isAttached = true;
         _collider = other;
         _dropPosition = other.transform.position;
     }
@@ -44,8 +75,14 @@ public class Drag : MonoBehaviour
         _collider = null;
         _dropPosition = _initTransformPosition;
     }
-     
-     
+
+    RootPlacementState placementState = RootPlacementState.Dragging;
+
+
+    bool _isAttached = false;
+
+    Vector3 _prevMousePosition;
+
     Collider _collider;
     Vector3 _initMousePosition;
     Vector3 _initTransformPosition;
@@ -53,4 +90,9 @@ public class Drag : MonoBehaviour
 
 }
 
-
+public enum RootPlacementState
+{
+    Dragging,
+    Rotating,
+    Locked
+}
