b86aad8d065eb116893b5657714e3a67c0eef453
Ricardo Bustamante <ricardo@busta.dev>
2023-02-04 20:39:29 +0100

    Set local position, not world position

--- Assets/Scripts/RootConnection.cs
+++ Assets/Scripts/RootConnection.cs
@@ -10,8 +10,8 @@ namespace GGJ23
         private void Awake()
         {
             InitConnection();
-            mesh.transform.position = Vector3.zero;
-            nodeGizmo.transform.position = Vector3.zero;
+            mesh.transform.localPosition = Vector3.zero;
+            nodeGizmo.transform.localPosition = Vector3.zero;
         }
 
         public void InitConnection()
