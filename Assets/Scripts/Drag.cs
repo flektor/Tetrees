622d2879a213f78c0ed6ea7c43130a86ff9f4e08
flektor <gdrosin@gmail.com>
2023-02-04 12:21:33 +0100

    First Commit

---
+++ Assets/Scripts/Drag.cs
@@ -0,0 +1,61 @@
+ using UnityEngine; 
+
+public class Drag : MonoBehaviour
+{
+
+    Vector3 prevMousePosition;
+ 
+
+
+    void OnMouseDown()
+    {
+        _initTransformPosition = transform.position;
+        _dropPosition = _initTransformPosition;
+        _initMousePosition = Input.mousePosition - Camera.main.WorldToScreenPoint(_initTransformPosition);
+    }
+
+    void OnMouseDrag()
+    {
+        if (_collider == null)
+        {
+
+            transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition - _initMousePosition);
+            return;
+        }
+
+        transform.position = _collider.transform.position;
+    }
+
+    void OnMouseUp()
+    {
+        transform.position = _dropPosition;
+        prevMousePosition = _dropPosition; 
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Node"))
+        {
+            _dropPosition = _initTransformPosition;
+            return;
+        }
+        _collider = other;
+        _dropPosition = other.transform.position;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        _collider = null;
+        _dropPosition = _initTransformPosition;
+    }
+     
+     
+    Collider _collider;
+    Vector3 _initMousePosition;
+    Vector3 _initTransformPosition;
+    Vector3 _dropPosition;
+
+}
+
+
