c43c383b898f9c69294246fdbd680272b2cf7b8b
Dominik <dominik.huebner@gmail.com>
2023-02-05 15:32:22 +0100

    Some shitty hold mechanic...

--- Assets/Scripts/RootSpawner.cs
+++ Assets/Scripts/RootSpawner.cs
@@ -9,10 +9,28 @@ namespace GGJ23
     {
         [SerializeField] private List<Root> _rootPrefabs = new();
         
+        private Root _holdingRoot;
+        
         public Root SpawnRoot()
         {
             var nodePrefab = _rootPrefabs[Random.Range(0, _rootPrefabs.Count)];
             return Instantiate(nodePrefab, new Vector3(10000, 0, 0), Quaternion.identity);
+        }
+
+        public Root HoldRoot(Root currentRoot)
+        {
+            if (!_holdingRoot)
+            {
+                _holdingRoot = SpawnRoot();
+                _holdingRoot.transform.position = new Vector3(20000, 0, 0);
+                _holdingRoot.transform.rotation = Quaternion.identity;
+            }
+            (currentRoot, _holdingRoot) = (_holdingRoot, currentRoot);
+            (currentRoot.transform.position, _holdingRoot.transform.position) = (_holdingRoot.transform.position, currentRoot.transform.position);
+            currentRoot.GetOutOfHold();
+            _holdingRoot.PutOnHold();
+            return currentRoot;
+
         }
     }
 }
