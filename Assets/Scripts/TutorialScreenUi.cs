f5ce78deb520185cffd9e1f32277af79f35010d8
Ricardo Bustamante <ricardo@busta.dev>
2023-02-05 15:11:30 +0100

    Prepare game for build, add scenes

---
+++ Assets/Scripts/TutorialScreenUi.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace GGJ23
+{
+    public class TutorialScreenUi : MonoBehaviour
+    {
+        [SerializeField] private Button _backButton;
+        
+        void Start()
+        {
+            _backButton.onClick.AddListener(() =>
+            {
+                SceneManager.LoadScene("StartScreen");
+            });
+        }
+    }
+}
